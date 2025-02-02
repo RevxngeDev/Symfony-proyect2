cf06801cf7c392b9c1036b6824ba4859f3e34b8c
RevxngE4 <andrespisa4@gmail.com>
2025-02-02 18:50:19 +0300

    todo hecho added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\FilmService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,11 +14,13 @@ class HomeController extends AbstractController
 {
     private Security $security;
     private FilmService $filmService;
+    private LoggerInterface $logger;
 
-    public function __construct(Security $security, FilmService $filmService)
+    public function __construct(Security $security, FilmService $filmService, LoggerInterface $logger)
     {
         $this->security = $security;
         $this->filmService = $filmService;
+        $this->logger = $logger;
     }
 
     #[Route('/home', name: 'app_home')]
@@ -25,10 +28,12 @@ class HomeController extends AbstractController
     {
         // Obtener el usuario autenticado
         $user = $this->security->getUser();
+        $username = $user ? $user->getUserIdentifier() : 'Guest';
+
+        $this->logger->info("El usuario '$username' accedió a la página de inicio.");
 
         $topLikedFilms = $this->filmService->getTopLikedFilms();
 
-        // Pasar los datos del usuario a la plantilla
         return $this->render('home/index.html.twig', [
             'user' => $user,
             'topLikedFilms' => $topLikedFilms,
@@ -38,6 +43,8 @@ class HomeController extends AbstractController
     #[Route('/api/home/films', name: 'api_home_films', methods: ['GET'])]
     public function getFilms(): JsonResponse
     {
+        $this->logger->info("Se solicitó la lista de películas desde la API.");
+
         $films = $this->filmService->getAllFilms();
         return $this->json($films);
     }
